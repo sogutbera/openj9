e66200775c85fcb0425b8608cff5eb8b36971eae
sogutbera <sogutbera@ibm.com>
2019-06-25 18:53:37 -0400

    added tests
    Signed-off-by: sogutbera <sogutbera@ibm.com>

--- test/functional/cmdLineTests/utils/src/org/openj9/test/lambdatests/Test2.java
+++ test/functional/cmdLineTests/utils/src/org/openj9/test/lambdatests/Test2.java
@@ -29,11 +29,11 @@ public class Test2 {
     public void func(){
         Comparator<String> strComparator = (String o1, String o2) -> o1.compareTo(o2);
         int strComparison = strComparator.compare("coffee", "cup");
-        System.out.print(strComparison);
+        System.out.println(strComparison);
             
         int x = 1134;
         Comparator<String> strComparator2= (String o1, String o2) -> o1.compareTo(o2)+o2.compareTo(o1)+1523+x;
         int strComparison2 = strComparator2.compare("a_cup_of", "tea");
-        System.out.print(strComparison2);
+        System.out.println(strComparison2);
     }
 }
